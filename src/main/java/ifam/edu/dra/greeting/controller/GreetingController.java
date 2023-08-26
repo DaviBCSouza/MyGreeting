505c19d2df8ad3be2c2ab73f82885632e6feaa79
DaviBCSouza <contato@davibrenno.com>
2023-08-26 01:11:07 -0400

    API Greeting - CRUD for Greeting

--- src/main/java/ifam/edu/dra/greeting/controller/GreetingController.java
+++ src/main/java/ifam/edu/dra/greeting/controller/GreetingController.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +33,40 @@ public class GreetingController {
 	public List<Greeting> getGreetings() {
 		return greeting;
 	}
+	
+	private boolean isValidIndex(int id) {
+		return id >= 0 && id < greeting.size();
+	}
 
+	@GetMapping("/greetings/{id}")
+	public Greeting getGreetingById(@PathVariable int id) {
+
+		if (isValidIndex(id)) {
+			return greeting.get(id);
+		} else {
+			throw new IndexOutOfBoundsException("Índice Inválido");
+		}
+
+	}
+
+	@PutMapping("/greetings/update/{id}")
+	public Greeting updateGreetingById(@PathVariable int id, @RequestBody String newName) {
+		if (isValidIndex(id)) {
+			Greeting greetingToUpdate = greeting.get((int) id);
+			greetingToUpdate.setName(newName);
+			return greetingToUpdate;
+		} else {
+			throw new IndexOutOfBoundsException("Índice Inválido");
+		}
+	}
+	
+	@DeleteMapping("/greetings/delete/{id}")
+	public String deleteGreetingById(@PathVariable int id) {
+	    if (isValidIndex(id)) {
+	        greeting.remove((int) id);
+	        return "Deletado a saudação com o ID: " + id;
+	    } else {
+	        throw new IndexOutOfBoundsException("Índice Inválido");
+	    }
+	}
 }
